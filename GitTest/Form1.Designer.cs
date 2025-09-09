68b1fcb56b9c5a14372f61b8c87ad511f39b2085
ims1440-lab <ims1440@gmail.com>
2025-09-09 11:37:29 +0900

    9/9 작업끝

--- GitTest/Form1.Designer.cs
+++ GitTest/Form1.Designer.cs
@@ -28,25 +28,25 @@
         /// </summary>
         private void InitializeComponent()
         {
-            button1 = new Button();
+            BtnClose = new Button();
             SuspendLayout();
             // 
-            // button1
+            // BtnClose
             // 
-            button1.Location = new Point(483, 118);
-            button1.Name = "button1";
-            button1.Size = new Size(162, 56);
-            button1.TabIndex = 0;
-            button1.Text = "button1";
-            button1.UseVisualStyleBackColor = true;
-            button1.Click += button1_Click;
+            BtnClose.Location = new Point(490, 204);
+            BtnClose.Name = "BtnClose";
+            BtnClose.Size = new Size(112, 55);
+            BtnClose.TabIndex = 0;
+            BtnClose.Text = "닫기";
+            BtnClose.UseVisualStyleBackColor = true;
+            BtnClose.Click += button1_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(9F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Controls.Add(button1);
+            Controls.Add(BtnClose);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
@@ -54,6 +54,6 @@
 
         #endregion
 
-        private Button button1;
+        private Button BtnClose;
     }
 }
